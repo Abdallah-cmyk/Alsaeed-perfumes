06d8187b0ee453ec2baddd62a1d7a16fa0830909
Abdullah <abdullahhamad010.00@gmail.com>
2026-03-12 08:44:02 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleCategory(el){
+
+let products=el.nextElementSibling
+
+if(products.style.display==="block"){
+products.style.display="none"
+}
+else{
+products.style.display="block"
+}
+
+}
